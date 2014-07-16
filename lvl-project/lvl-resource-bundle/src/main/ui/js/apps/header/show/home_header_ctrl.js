f941ad264dd665ec1c3098eebb09c8aacf120977
Erik Torres Serrano <etserrano@gmail.com>
2014-07-16 22:43:43 +0200

    Refine home header navigation template

--- lvl-project/lvl-resource-bundle/src/main/ui/js/apps/header/show/home_header_ctrl.js
+++ lvl-project/lvl-resource-bundle/src/main/ui/js/apps/header/show/home_header_ctrl.js
@@ -3,29 +3,20 @@
  */
 
 define([ 'app', 'apps/header/show/home_header_view' ], function(Lvl, View) {
-
     Lvl.module('HeaderApp.Home', function(Home, Lvl, Backbone, Marionette, $, _) {
         'use strict';
         Home.Controller = {
             showHeader : function() {
                 require([ 'entities/navigation' ], function() {
                     var links = Lvl.request('navigation:external:entities');
-
-                    // TODO
-                    console.log(JSON.stringify(links));
-                    // TODO
-
                     var view = new View.Header({
                         collection : links
                     });
-
-
                     Lvl.headerRegion.show(view);
                     return View.Header.id;
                 });
             }
         }
     });
-
     return Lvl.HeaderApp.Home.Controller;
 });
